52bb3a5d592a4e5e8990212a16892a04487da0e0
mToan <mtoanp@outlook.com>
2024-02-19 10:50:42 +0100

    hotfix: css

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
               <Route path="*" element={<h1>404</h1>} />
             </Routes>
             <UnsplashImages searchQuery={city} />
-            <ChatbotComponent />
+            {/* <ChatbotComponent /> */}
             <Footer />
           </Router>
         </ThemeProvider>
